5659bef631587db31d1b6217f98c4fb30e612d9e
CPB <christopher@paperspace.com>
2020-12-15 22:14:07 -0500

    Just fixing up the app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
+import logo from './PNG image 58.png';
 import './App.css';
 
 function App() {
+  const [value, setValue] = useState(null)
+  const [err, setErr] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  async function fetchQuotes() {
+    try {
+      setLoading(true)
+      const response = await fetch("https://type.fit/api/quotes")
+      const quotes = await response.json();
+      const randNum = Math.floor(Math.random() * (quotes.length))
+      const randQuote = quotes[randNum].text
+      const randAuth = quotes[randNum].author
+      const value = (
+        <div
+          style={{
+            width: '60%',
+            margin: 'auto',
+            backgroundColor: 'purple',
+            padding:'20px',
+            minHeight: '100px'
+          }}
+        >
+          <div> {randQuote} </div>
+          <br />
+          <div> {randAuth} </div>
+        </div>
+      )
+      setValue(value)
+    } catch (e) {
+      setErr(e)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchQuotes()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> to get cool quotes at random
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Quote Generator</h1>
+        <button type='button' onClick={fetchQuotes}> Quote Me </button>
+        <p>
+          {loading ? 'loading...' : value}
+        </p>
       </header>
     </div>
   );
